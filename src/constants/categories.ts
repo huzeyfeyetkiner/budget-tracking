cd138a33f5788a6b6cdff383934b549c0e6d418f
Huzeyfe Yetkiner <yetkinerhuzeyfe@gmail.com>
2024-11-25 21:17:01 +0300

    data: caregories data

---
+++ src/constants/categories.ts
@@ -0,0 +1,55 @@
+export const categories = [
+	{
+		id: 1,
+		name: "Gıda",
+	},
+	{
+		id: 2,
+		name: "Eğitim",
+	},
+	{
+		id: 3,
+		name: "Sağlık",
+	},
+	{
+		id: 4,
+		name: "Kira",
+	},
+	{
+		id: 5,
+		name: "Fatura",
+	},
+	{
+		id: 6,
+		name: "Ulaşım",
+	},
+
+	{
+		id: 7,
+		name: "Eğlence",
+	},
+	{
+		id: 8,
+		name: "Spor",
+	},
+	{
+		id: 9,
+		name: "Hediye",
+	},
+	{
+		id: 10,
+		name: "Kıyafet",
+	},
+	{
+		id: 11,
+		name: "Araç",
+	},
+	{
+		id: 12,
+		name: "Ev",
+	},
+	{
+		id: 13,
+		name: "Diğer",
+	},
+]
